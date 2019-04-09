278fb78ca4dfddc5756c243052e3eee87ed8be24
王玲峰 <31333595+Wlfade@users.noreply.github.com>
2019-04-09 17:44:53 +0800

    1.test
    1.提交文件

---
+++ 1.test2/1.test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1.test2
+//
+//  Created by 单车 on 2019/4/9.
+//  Copyright © 2019 单车. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
